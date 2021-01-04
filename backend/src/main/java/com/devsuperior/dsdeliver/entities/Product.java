0d6ac787c849155da86ee698c2a2945f25de26be
Gabriel <koymattk@gmail.com>
2021-01-04 15:20:37 -0300

    GET /products

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
@@ -20,6 +20,9 @@ public class Product implements Serializable {
 	private String description;
 	private String imageUri;
 	
+	public Product() {
+		
+	}
 	public Product(Long id, String name, Double price, String description, String imageUri) {
 		super();
 		this.id = id;
