ae5b125a4166e073e5d303640971cb8d70d4ee4a
Gabriel <koymattk@gmail.com>
2021-01-06 13:38:23 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import Navbar from './NavBar';
 
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar/>
-    </div>
+      <Home/>
+    </>
   );
 }
 
