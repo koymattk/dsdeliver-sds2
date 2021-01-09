25fbfa2e2213154e2e54d4347dd86cc5221f76b7
Paulo G G Matias <65675489+koymattk@users.noreply.github.com>
2021-01-09 14:06:57 -0300

    Add files via upload

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from '../api';
-import { OrderLocationdata } from './types';
+import { OrderLocationData } from './types';
 
 const initialPosition = {
     lat: -10.853308908352348,
@@ -19,7 +19,7 @@ type Place = {
 }
 
 type Props = {
-    onChangeLocation: (location: OrderLocationdata) => void;
+    onChangeLocation: (location: OrderLocationData) => void;
 }
 const OrderLocation = ({ onChangeLocation }:Props) => { 
     const[address, setAddress] = useState<Place>({position:initialPosition});
